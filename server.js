bcac4a409f6e95f9b5c81d4092bbe681951e0ef3
Bryan <ez761688@gmail.com>
2024-03-07 18:08:42 +0800

    設置回傳表頭JSON格式

--- server.js
+++ server.js
@@ -3,20 +3,26 @@ const http = require("http");
 const requestListener = (req, res) => {
 
   const headers = {
-    "Content-Type": "text/plain"
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
   }
 
   console.log(req.url);
 
   if (req.url === "/" && req.method == "GET") {
     res.writeHead(200, headers);
-    res.write("請求成功");
+    res.write(JSON.stringify({
+        "status":"success",
+         data:[]
+    }));
     res.end();
-  }else if (req.url === "/" && req.method == "DELETE") {
+  } else if (req.url === "/" && req.method == "DELETE") {
     res.writeHead(200, headers);
     res.write("刪除成功");
     res.end();
-  } else{
+  } else {
     res.writeHead(404, headers);
     res.write("輸入錯誤");
     res.end();
