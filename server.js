c2c8ae584a0d301ed01e84efbc72bae20deebba4
Bryan <ez761688@gmail.com>
2024-03-07 15:53:01 +0800

    新增404方式

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const http =  require("http");
+const http = require("http");
 
-const requestListener = (req,res)=>{
-    // console.log(req);
-    console.log(req.url);
-    res.writeHead(200,{"Content-Type":"text/plain"});
-    res.write("Hello 123");
+const requestListener = (req, res) => {
+
+  const headers = {
+    "Content-Type": "text/plain"
+  }
+
+  console.log(req.url);
+
+  if (req.url === "/" && req.method == "GET") {
+    res.writeHead(200, headers);
+    res.write("請求成功");
     res.end();
+  }else if (req.url === "/" && req.method == "DELETE") {
+    res.writeHead(200, headers);
+    res.write("刪除成功");
+    res.end();
+  } else{
+    res.writeHead(404, headers);
+    res.write("輸入錯誤");
+    res.end();
+  }
 }
 
 const server = http.createServer(requestListener);
